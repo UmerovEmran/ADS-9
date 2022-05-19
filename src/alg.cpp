b9235a7546735708c3abc9f3544c6f95cd152c76
UmerovEmran <99113996+UmerovEmran@users.noreply.github.com>
2022-05-19 18:14:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,27 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> itog;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return itog;
+  }
+  char prom;
+  std::string prov = "";
+  while (!file.eof()) {
+    prom = file.get();
+    if (prom <= 'Z' && prom >= 'A') {
+      prom = prom + ('a' - 'A');
+    }
+    if (prom <= 'z' && prom >= 'a') {
+      prov += prom;
+    } else {
+        itog.add(prov);
+        prov = "";
+      }
+  }
+  return itog;
 }
+
